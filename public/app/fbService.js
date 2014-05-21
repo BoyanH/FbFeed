19a95dcfa8ff8b1ce1a30c91e8972284e52dbddb
unknown <alexanderyordanov96@gmail.com>
2014-05-21 20:40:34 +0300

    Fixed login permissions

--- public/app/fbService.js
+++ public/app/fbService.js
@@ -1,9 +1,10 @@
-﻿app.factory("FacebookService", function ($location) {
+﻿app.factory("FacebookService", function ($location, $q) {
     FB.init({
         appId: '1480652358834115',
         status: true, // check login status
         cookie: true, // enable cookies to allow the server to access the session
-        xfbml: true // parse XFBML
+        xfbml: true, // parse XFBML
+        read_stream: true
     });
     console.log('FB initialized...');
     return {
@@ -15,7 +16,8 @@
                     console.log('Logged in.');
                 }
                 else {
-                    FB.login();
+                    FB.login(function(response){},
+                        {scope:'user_status,read_stream,publish_stream,user_likes,publish_actions,read_friendlists,rsvp_event'});
                     console.log('Now logged in.');
                 }
                 $location.path('/home');
@@ -25,6 +27,20 @@
             FB.getLoginStatus(function (response) {
                 return response.status;
             })
+        },
+        getPages: function(){
+            var deferred = $q.defer();
+            FB.api(
+                "/me/home",
+                function ( response ) {
+                    console.log(response);
+                    if ( response && !response.error ) {
+                        console.log(response);
+                        deferred.resolve(response);
+                    }
+                }
+            );
+            return deferred.promise;
         }
     }
 })
--- public/app/main/mainController.js
+++ public/app/main/mainController.js
@@ -1,7 +1,5 @@
 ﻿app.controller('MainController', function($scope, FacebookService){
-    $scope.loggedIn = false;
     $scope.login = function(){
         FacebookService.login();
-        $scope.loggedIn=true;
     };
 });
