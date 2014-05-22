7ed6ff8bab40c4347b97bc285ef1d8e03ae864b3
BoyanH <boqnh@abv.bg>
2014-05-22 23:34:56 +0300

    made embedding in pages view the same like I did in videos

--- public/app/pages/pageController.js
+++ public/app/pages/pageController.js
@@ -1,8 +1,26 @@
-﻿app.controller('PagesController', function ($scope, FacebookService, PopupService) {
+﻿app.controller('PagesController', function ($scope, FacebookService, PopupService, EmbedService) {
     $scope.stillLoding = true;
     FacebookService.getPages().then(function (data) {
         $scope.pageImages = [];
         $scope.pages = data;
+
+        for(var z = 0; z < $scope.pages.length; z++) {
+
+            if(pages[z].application) {
+
+                    var type = pages[z].application.name;
+                    if(type == 'Video' || type == 'YouTube') {
+
+                        $scope.pages[z].embedLink = EmbedService.normalizeLink($scope.pages[z]);
+                    }
+            }
+
+            else if(pages[z].type == 'video') {
+
+                $scope.pages[z].embedLink = EmbedService.normalizeLink($scope.pages[z]);
+            }
+        }
+
         var k = 0;
         $scope.appID = FacebookService.getAppID();
         function profileImageLoop() {
@@ -12,14 +30,19 @@
                     if (k < data.length) {
                         setTimeout(profileImageLoop, 1);
                     }
-                })
+                });
         }
         profileImageLoop();
         for (var t = 0; t < data.length; t++) {
-            if(data[t].type=="photo")
+            if(data[t].type=="photo") {
                 $scope.pages[t].postPhoto = "https://graph.facebook.com/" + data[t].object_id + "/picture";
+            }
         }
         $scope.stillLoding = false;
         console.log(data);
+
+        $scope.trustSrc = function(src) {
+            return $sce.trustAsResourceUrl(src);
+        }
     });
 });
