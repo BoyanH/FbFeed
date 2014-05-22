4d502473db6e9ec73bc662a5bdb8a18f2dc38141
unknown <alexanderyordanov96@gmail.com>
2014-05-23 01:20:40 +0300

    Some changes in the UI in statuses but not working perfectly correct

--- public/app/statuses/statusController.js
+++ public/app/statuses/statusController.js
@@ -1,15 +1,26 @@
 app.controller('StatusController', function($scope, FacebookService){
-    
+    $scope.stillLoding = true;
     FacebookService.getStatuses().then(function(data){
         
         console.log(data);
         $scope.allStatuses = data;
         $scope.statuses = $scope.allStatuses.splice(0, 10);
+        var k = 0;
+        function profileImageLoop() {
+            FacebookService.getPictureByID(data[k].from.id)
+                .then(function (url) {
+                    $scope.statuses[k++].profileImage = url;
+                    if (k < data.length) {
+                        setTimeout(profileImageLoop, 1);
+                    }
+                });
+        }
+        profileImageLoop();
+        $scope.stillLoding = false;
     });
 
     $scope.changePage = function (page) {
 
     	$scope.statuses = $scope.allStatuses.splice(0, page*10);
     }
-
 });
