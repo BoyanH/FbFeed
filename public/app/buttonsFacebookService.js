d2b1b7ec869255d723a6c0afd3e188f3772fedeb
unknown <alexanderyordanov96@gmail.com>
2014-05-23 20:49:21 +0300

    Fixed bug with undefined items in route pages

---
+++ public/app/buttonsFacebookService.js
@@ -0,0 +1,35 @@
+﻿app.factory( 'ButtonsFacebookService', function () {
+    
+    return {
+        share: function (item) {
+
+        	if(item.link.substring(0, 25) == 'https://www.facebook.com/' && item.type == 'photo') {
+
+        		item.picture = 'https://images.weserv.nl/?url=' + item.postPhoto.substring(8);
+        		item.link = item.picture;
+        	}
+
+    		console.log(item);
+        	FB.ui(
+			  {
+			    method: 'share',
+			    href: item.link,
+			    picture: item.picture,
+			    caption: item.caption,
+			    name: item.name,
+			    status_type: item.status_type,
+			    type: item.type,
+			    description: item.description
+			  },
+			  function(response) {
+			    if (response && !response.error_code) {
+			      alert('Posting completed.');
+			    } else {
+			      alert('Error while posting.');
+			    }
+			  }
+			);
+
+        }
+    }
+});
--- public/app/pages/pageController.js
+++ public/app/pages/pageController.js
@@ -1,28 +1,19 @@
-﻿app.controller('PagesController', function ($scope, $sce, FacebookService, PopupService, EmbedService) {
+﻿app.controller('PagesController', function ($scope, $sce, FacebookService, ButtonsFacebookService, EmbedService) {
     $scope.stillLoding = true;
     FacebookService.getPages().then(function (data) {
         $scope.pageImages = [];
-        $scope.pages = data;
-
-        for(var z = 0; z < $scope.pages.length; z++) {
-
-            if($scope.pages[z].application) {
-
-                    var type = $scope.pages[z].application.name;
-                    if(type == 'Video' || type == 'YouTube') {
-
-                        $scope.pages[z] = EmbedService.normalizeLink($scope.pages[z]);
-                    }
-            }
-
-            else if($scope.pages[z].type == 'video') {
-
-                $scope.pages[z] = EmbedService.normalizeLink($scope.pages[z]);
+        var pages = [];
+        for(var i=0;i<data.length;i++){
+            if(data[i]){
+                pages.push(data[i]);
             }
         }
+        data = pages;
+        $scope.pages = data;
 
         var k = 0;
         function profileImageLoop() {
+            if(data[k]){
             FacebookService.getPictureByID(data[k].from.id)
                 .then(function (url) {
                     $scope.pages[k++].profileImage = url;
@@ -30,10 +21,17 @@
                         setTimeout(profileImageLoop, 1);
                     }
                 });
+            }
+            else{
+                if(k<data.length){
+                    k++;
+                    setTimeout(profileImageLoop, 1);
+                }
+            }
         }
         profileImageLoop();
         for (var t = 0; t < data.length; t++) {
-            if(data[t].type=="photo") {
+            if(data[t] && data[t].type=="photo") {
                 $scope.pages[t].postPhoto = "https://graph.facebook.com/" + data[t].object_id + "/picture";
             }
         }
@@ -43,10 +41,26 @@
         $scope.trustSrc = function(src) {
             return $sce.trustAsResourceUrl(src);
         }
+        for(var z = 0; z < data.length; z++) {
+
+            if(data[z] && data[z].application) {
+
+                    var type = data[z].application.name;
+                    if(type == 'Video' || type == 'YouTube') {
+                        console.log(EmbedService.normalizeLink(data[z]));
+                        $scope.pages[z] = EmbedService.normalizeLink(data[z]);
+                    }
+            }
+
+            else if(data[z] && $scope.pages[z].type == 'video') {
+
+                $scope.pages[z] = EmbedService.normalizeLink($scope.pages[z]);
+            }
+        }
     });
 
     $scope.share = function (item) {
 
-        PopupService.share(item);
+        ButtonsFacebookService.share(item);
     }
 });
