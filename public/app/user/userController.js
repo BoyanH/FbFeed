33e3ff6c67eab190af52783dd2e2d46c2e2bcad1
unknown <alexanderyordanov96@gmail.com>
2014-05-27 02:37:14 +0300

    Implemented name on nav

---
+++ public/app/user/userController.js
@@ -0,0 +1,6 @@
+﻿app.controller('UserController', function($scope, FacebookService, $rootScope){
+    FacebookService.getAuthData()
+        .then(function (data) {
+            $rootScope.user = data;
+    })
+});
--- public/app/userDirective/userDirective.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-app.directive('userDirective', function() {
-    return {
-        restrict: 'A',
-        template: '<a class="user-acc" href="{{user.link}}" target="_blank">{{user.first_name}} {{user.last_name}}</a>'
-    }
-})
