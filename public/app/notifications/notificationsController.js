db9e5dcdfa10ce2363a9f357f29b88501697d6ed
BoyanH <boqnh@abv.bg>
2014-06-05 23:51:25 +0300

    styled the notification box, fixed error with profile pics in notifications

--- public/app/notifications/notificationsController.js
+++ public/app/notifications/notificationsController.js
@@ -1,13 +1,39 @@
 app.controller('NotificationsController', function($scope, FacebookService, $rootScope){
 
     var checkNotifications = setInterval(getNotifications, 2000);
-
     function getNotifications () {
 
     	FacebookService.getUserNotifications().then(function(response) {
 
-	    	$scope.notifications = response.data;
-            console.log(response.data);
+            if($scope.notifications) {
+
+                if($scope.notifications.forEach(function(item) { if(!item.profileImage) return true})) {
+
+                    var k = 0;
+                    $scope.notifications = response.data;
+                    profileImageLoop();
+                }
+            }
+                else{
+
+                    var k = 0;
+                    $scope.notifications = response.data;
+                    profileImageLoop();
+                }
+
+            function profileImageLoop() {
+
+                if ( response.data[k] ) {
+                    FacebookService.getPictureByID( response.data[k].from.id )
+                        .then( function ( url ) {
+                            $scope.notifications[k++].profileImage = url;
+                            if ( k < response.data.length ) {
+                                setTimeout( profileImageLoop, 1 );
+                            }
+                        });
+
+                }
+            }
 	    });
 
     }
